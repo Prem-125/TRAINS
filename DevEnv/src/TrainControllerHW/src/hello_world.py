7f6b46a62b10ca90a0aaf2272bb1f1a7978ceb35
Prem Bharatia <prem.bharatia@gmail.com>
2021-02-27 14:53:15 -0500

    temp commit

--- DevEnv/src/TrainControllerHW/src/hello_world.py
+++ DevEnv/src/TrainControllerHW/src/hello_world.py
@@ -1,33 +1,29 @@
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+import serial
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QFile, QTimer
+from test1 import Ui_TrainModelInterface
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText("Test123")
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		self.ui = Ui_TrainModelInterface()
+		self.ui.setupUi(self)
+		self.utimer = QTimer()
+		self.utimer.timeout.connect(self.updateVal)
+		self.utimer.start(1000)
+		self.testval=0
+		#self.arduino = serial.Serial(port='COM4', baudrate=115200)
 		
-		
+	def updateVal(self):
+		#status = self.arduino.read(2)	
+		self.testval = self.testval + 1
+		self.ui.PowerVal.setPlainText(str(self.testval))
+
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+	app = QApplication(sys.argv)
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+	window = MainWindow()
+	window.show()
 
-    sys.exit(app.exec_())
+	sys.exit(app.exec_())
