5435bc3bd4871c166d98d8b86ab89c2fd193a182
Prem Bharatia <prem.bharatia@gmail.com>
2021-03-02 22:33:54 -0500

    Did work wit Aurimas

---
+++ DevEnv/src/TrainControllerHW/src/arduino/IO_Test/IO_Test.ino
@@ -0,0 +1,280 @@
+#include "pinMaps.h"
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 20 chars and 4 line display
+unsigned int ToggleBtnStates = 0;
+unsigned int ToggleStates = 0;
+unsigned int CntrlBtnStates = 0; 
+int LCDViewIndex = 6;
+
+void LCDSpeed();
+void LCDBeacon();
+void LCDTemp();
+void LCDPower();
+void LCDAuth();
+void SendAnnounce();
+void TempUp();
+void TempDown();
+void SpeedUp();
+void SpeedDown();
+
+void (*funcSel[10])() = { SpeedDown , SpeedUp, SendAnnounce,  TempUp, LCDBeacon, LCDTemp, 
+                        LCDSpeed, LCDAuth, LCDPower, TempDown };
+
+
+char readbuff[70]; 
+float cmdVel = 150.0;
+float curVel = 0.0;
+float setpointVel = 100;
+float auth = 0.0;
+bool refresh = true;
+long long TCEnc;
+int beaconEnc;
+int temperature = 72;
+int flags; 
+bool LDoorsOpen;
+bool RDoorsOpen;
+bool ExtLightsOn;
+bool UpcomingStation;
+const String  Stations[] = {"Shadyside","Herron Ave","Swissville","Penn Station","Steel Plaza","First Ave","Station Square","South Hills Junction", 
+                            "Pioneer","Edgebrook","Whited","South Bank","Central","Inglewood","Overbrook","Glenburry","Dormont","Mt Lebanon", "Poplar","Castle Shannon"};
+String announcement = "";
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(1);
+  pinSetup();
+  for (int i = 0; i < NUMLED; i++)
+  digitalWrite(LEDs[i],LOW);
+  lcd.init();  //initialize the lcd
+  lcd.backlight();  //open the backlight 
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readToggleBtns();
+  updateToggleStates();
+  updateToggleLEDs();
+  readCntrlBtns();
+  processCntrlBtns();
+  serialRead();
+  if(refresh){
+  (*funcSel[LCDViewIndex])(); // Refreshes LCD incase data changed from serial comms 
+  refresh = false;
+  }
+
+//  lcd.clear();
+//  lcd.setCursor(0,0);
+//  lcd.print(ToggleBtnStates);
+//  lcd.setCursor(0,1);
+//  lcd.print(CntrlBtnStates);
+  delay(400);
+}
+
+void pinSetup(){
+
+// buttons  all use the internal pullup, inverted logic
+for (int i =0; i < NUMBTN; i++)
+  pinMode(Buttons[i],INPUT_PULLUP);
+
+//Leds 
+for (int i = 0; i < NUMLED; i++)
+  pinMode(LEDs[i],OUTPUT);
+
+}
+
+void readToggleBtns(){
+  ToggleBtnStates = 0;
+  int temp = 0;
+  for (int i =0; i < NUMTOGGLE; i++){
+      //Serial.println((!(digitalRead(Buttons[i])& 1)));
+      ToggleBtnStates += long(!(digitalRead(Buttons[i])& 1)) << i;
+  }
+}
+
+void readCntrlBtns(){
+  CntrlBtnStates = 0;
+  int temp = 0;
+  for (int i =NUMTOGGLE; i < NUMBTN; i++){
+      //Serial.println((!(digitalRead(Buttons[i])& 1)));
+      CntrlBtnStates += long(!(digitalRead(Buttons[i])& 1)) << i-NUMTOGGLE;
+  }
+}
+
+void updateToggleStates(){
+    ToggleStates ^= (ToggleBtnStates & int(pow(2,NUMTOGGLE)- 1)); 
+}
+
+void updateToggleLEDs() {
+  for (int i = 0; i < NUMTOGGLE; i++){
+    digitalWrite(LEDs[i],(ToggleStates >> i) & 1);
+  }
+  
+}
+
+void processCntrlBtns(){
+  for (int i = 0; i < NUMBTN - NUMTOGGLE; i++){
+    if(((CntrlBtnStates >> i) & 1) == 1){
+      LCDViewIndex = i;
+      (*funcSel[i])();
+      
+      break;
+    }
+  }
+}
+
+
+void LCDSpeed(){
+   lcd.clear();
+  lcd.setCursor(0,0);
+   lcd.setCursor ( 0, 0 );          
+  lcd.print("Cmd Vel: "); 
+  lcd.setCursor ( 10, 0 );            
+  lcd.print(cmdVel);
+  lcd.setCursor(0,1);
+  lcd.print("Setpoint Vel: ");
+  lcd.setCursor(14,1);
+  lcd.print(setpointVel); 
+  lcd.setCursor(0,2);
+  lcd.print("Cur Vel: ");
+  lcd.setCursor(9,2);
+  lcd.print(curVel);  
+}
+void LCDBeacon(){
+   lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(announcement);
+}
+void LCDTemp(){
+   lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Set Temp is:");
+    lcd.setCursor(0,1);
+  lcd.print(temperature);
+}
+void LCDPower(){
+   lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("LCD Power");
+}
+void LCDAuth(){
+   lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("Auth: ");
+  lcd.setCursor(6,1);
+  lcd.print(auth);
+}
+void SendAnnounce(){
+   lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("LCD Send Announce");
+}
+void TempUp(){
+  if (temperature < 80){
+    temperature+=1;
+  }else {
+    temperature = 80;
+  }
+   LCDViewIndex = 5;
+  refresh = true;
+}
+void TempDown(){
+  if (temperature > 60){
+    temperature-=1;
+  }else {
+    temperature = 60;
+  }
+   LCDViewIndex = 5;
+  refresh = true;
+}
+void SpeedUp(){
+  if (setpointVel < cmdVel - 2.5){
+    setpointVel+=2.5;
+  }else {
+    setpointVel = cmdVel;
+  }
+  LCDViewIndex = 6;
+  refresh = true;
+
+ 
+}
+void SpeedDown(){
+    if (setpointVel >2.5){
+    setpointVel-=2.5;
+  }else {
+    setpointVel = 0;
+  }
+  LCDViewIndex = 6;
+  refresh = true;
+}
+
+long long atoll(const char* ptr) {
+  long long result = 0;
+  while (*ptr && isdigit(*ptr)) {
+    result *= 10;
+    result += *ptr++ - '0';
+  }
+  return result;
+}
+
+void decodeTC(){
+  unsigned int cmdInt = TCEnc & 255;
+  unsigned int cmdFlt = (TCEnc >> 8) & 15;
+  unsigned int authInt = (TCEnc >> 12) & 255;
+  unsigned int authFlt = (TCEnc >> 20) & 15;
+  unsigned int check = (TCEnc >> 24) & 1023;
+  digitalWrite(LEDs[9],LOW);
+  if((cmdInt + cmdFlt + authInt + authFlt) != check){
+    digitalWrite(LEDs[9],HIGH);
+    cmdVel = 0;
+    auth = 0;
+    return;
+  }
+  cmdVel = float(cmdInt) + float(cmdFlt) / 10.0;
+  auth = float(authInt) + float(authFlt) / 10.0;
+}
+
+void decodeBeacon(){
+  UpcomingStation = beaconEnc & 1;
+  LDoorsOpen = (beaconEnc >> 1) & 1;
+  RDoorsOpen = (beaconEnc >> 2) & 1;
+  ExtLightsOn = (beaconEnc >> 3) & 1;
+  String station = Stations[((beaconEnc >> 4) & 31)];
+  if(UpcomingStation){
+  announcement = "Arriving at " + station + " Station. The doors will open on the ";
+  if(LDoorsOpen && RDoorsOpen){
+    announcement += "Left and Right.\n";
+  }else if (LDoorsOpen){
+    announcement += "Left.\n";
+  }else{
+    announcement += "Right.\n";
+  }
+  }
+}
+
+void serialRead(){
+  while(Serial.available() > 0){
+    int numRead = Serial.readBytesUntil('\n',readbuff,20);
+    int sel = atoi(readbuff);
+    memset(readbuff,0,sizeof readbuff); // clear buffer
+    if (numRead == 0 || sel < 1 || sel > 3)
+      continue; 
+    delay(10);
+    numRead = Serial.readBytesUntil('\n',readbuff,70);
+    switch(sel){
+      case 1: // Got CurVel
+              curVel = atof(readbuff);
+              break;
+      case 2: // Got TC
+              TCEnc = atoll(readbuff);
+              decodeTC();
+              break;
+      default:// Got Beacon
+              beaconEnc = atoi(readbuff);
+              decodeBeacon();
+  }
+      memset(readbuff,0,sizeof readbuff); // clear buffer
+      refresh = true; 
+  }
+}
--- DevEnv/src/TrainControllerHW/src/hello_world.py
+++ DevEnv/src/TrainControllerHW/src/hello_world.py
@@ -1,8 +1,9 @@
 import sys
 import serial
+import time
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile, QTimer
-from test1 import Ui_TrainModelInterface
+from UI import Ui_TrainModelInterface
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -10,20 +11,98 @@ class MainWindow(QMainWindow):
 		self.ui = Ui_TrainModelInterface()
 		self.ui.setupUi(self)
 		self.utimer = QTimer()
-		self.utimer.timeout.connect(self.updateVal)
+		self.utimer.timeout.connect(self.timerCallback)
 		self.utimer.start(1000)
-		self.testval=0
-		#self.arduino = serial.Serial(port='COM4', baudrate=115200)
+		self.testval = 0
+		self.arduino = serial.Serial(port='COM3', baudrate=115200,timeout=1)
+		self.ui.tcButton.clicked.connect(self.getTCInfo)
+		self.ui.beaconButton.clicked.connect(self.getBeaconInfo)
+		self.ui.speedButton.clicked.connect(self.getCurSpeed)
+
+		self.cmdSpeed = ''
+		self.curSpeed = ''
+		self.authority = ''
+		self.encodedB=0
+		self.nFlag=0
+		self.eol = '\n'.encode('utf-8')
+		self.encodedTC=0
 		
-	def updateVal(self):
-		#status = self.arduino.read(2)	
-		self.testval = self.testval + 1
-		self.ui.PowerVal.setPlainText(str(self.testval))
+		
+	def timerCallback(self):
+		#self.arduino.write(str(self.testval).encode('utf-8')+ self.eol)
+		#self.serialWrite()
+		#self.serialRead()
+		...
+		
+		
+	def serialRead(self):
+		print(self.arduino.in_waiting)
+		raw = self.arduino.readline()
+		#print(raw)
+		status = raw.decode('ascii')#.strip('\r\n')
+		print(status)
+		self.ui.AnnounceVal.setPlainText(status)
+		self.arduino.reset_input_buffer()
+		
+	def serialWrite(self):
+		self.arduino.reset_output_buffer()
+		self.arduino.write(str(self.nFlag).encode('utf-8')+ self.eol)
+		self.arduino.write(str(self.encodedTC).encode('utf-8')+ self.eol)
+		self.arduino.write(self.curSpeed.encode('utf-8')+ self.eol)
+		self.arduino.write(str(self.encodedB).encode('utf-8')+ self.eol)
+		self.nFlag=0
+
+		
+		
+	def getTCInfo(self):
+		self.cmdSpeed= self.ui.CommSpeedVal.toPlainText()
+		self.curSpeed= self.ui.CurSpeedVal.toPlainText()
+		self.authority= self.ui.AuthVal.toPlainText()
+		cmdInt= int(float(self.cmdSpeed))
+		cmdFloat= int(((float(self.cmdSpeed)-cmdInt)*10))
+		authInt= int(float(self.authority))
+		authFloat= int(((float(self.authority)-authInt)*10))
+		if(self.ui.sigFail.checkState()):
+			self.encodedTC = (cmdInt-6 & 255)
+		else:
+			self.encodedTC = (cmdInt & 255)
+		self.encodedTC += (cmdFloat & 15) << 8
+		self.encodedTC += (authInt & 255) << 12
+		self.encodedTC += (authFloat & 15)<< 20
+		self.encodedTC += ((cmdInt + cmdFloat + authFloat + authInt) & 1023) << 24
+		
+		#print(bin(cmdInt))
+		#print(self.encodedTC)
+		print(bin(self.encodedTC))
+		self.nFlag|=2
+		#print(self.cmdSpeed)
+		self.arduino.write(str(2).encode('utf-8')+ self.eol)
+		self.arduino.write(str(self.encodedTC).encode('utf-8')+ self.eol)
+
+	def getBeaconInfo(self):
+		beaconNum= self.ui.stationSel.currentIndex()
+		print(int(self.ui.stationComing.checkState()))
+		self.encodedB = int(self.ui.stationComing.checkState()  )>> 1
+		print(bin(self.encodedB))
+		self.encodedB += (int(self.ui.BLDoor.checkState()) >> 1) << 1
+		self.encodedB += (int(self.ui.BRDoor.checkState()) >> 1) << 2
+		self.encodedB += (int(self.ui.BExtLight.checkState()) >> 1) << 3
+		self.encodedB += (beaconNum & 31) << 4
+		print(bin(self.encodedB))		
+		self.nFlag|=1
+		self.arduino.write(str(3).encode('utf-8')+ self.eol)
+		self.arduino.write(str(self.encodedB).encode('utf-8')+ self.eol)
+
+	
+	def getCurSpeed(self):
+		self.curSpeed= self.ui.CurSpeedVal.toPlainText()
+		self.arduino.write(str(1).encode('utf-8')+ self.eol)
+		self.arduino.write(self.curSpeed.encode('utf-8')+ self.eol)
+
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 
 	window = MainWindow()
 	window.show()
-
 	sys.exit(app.exec_())
