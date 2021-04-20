f279a9d00c93cbdd3e819142b943e94130ed0bcc
zbenning <zjbenning@gmail.com>
2021-04-20 16:16:20 -0400

    First Interface Test

--- DevEnv/src/TrackControllerSW/src/TrackControllerSW.py
+++ DevEnv/src/TrackControllerSW/src/TrackControllerSW.py
@@ -1,16 +1,11 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+""" from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
-from .UI import Ui_TrackControllerUI
+from .UI import Ui_TrackControllerUI """
 from signals import signals
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        #call parent constructor
-        super(MainWindow, self).__init__()
-        self.ui = Ui_TrackControllerUI()
-        self.ui.setupUi(self)
-        
+class TrackController:
+    def __init__(self, offset):
         #Variables
         self.block_open = [True for i in range(150)]
         self.occupancy = [False for i in range(150)]
@@ -19,27 +14,13 @@ class MainWindow(QMainWindow):
         self.commanded_speed = [0 for i in range(150)]
         self.authority_block = 0
         self.switch_state = True
-        self.block_offset = 0
+        self.block_offset = offset
         self.switch_exit_num = 0
         self.switch_in_a = 0
         self.switch_in_b = 0
         self.block_authority = 0
-
         #UI used variables
         self.ui_block = 0
-        self.plc_name = ""
-        
-        #UI Functions
-        self.ui.BlockInput.currentTextChanged.connect(self.UIBlockOutput)
-        self.ui.ImportButton.clicked.connect(self.ImportPLC)
-
-        #Signal Functions
-        signals.track_model_occupancy.connect(self.getOccupancy)
-        signals.CTC_authority.connect(self.getAuthority)
-        signals.track_break.connect(self.setBlockClosure)
-        signals.wayside_block_status.connect(self.UpdateBlockStatus)
-        #signals.CTC_suggested_speed.connect(self.getSugSpeed)
-        #need wayside to track switch signals
 
     #Sets the block offset for the track controller
     def setBlockOffset(self, b_offset):
@@ -49,7 +30,6 @@ class MainWindow(QMainWindow):
     def getOccupancy(self, block_num, occupied):
         self.getSugSpeed(block_num)
         self.occupancy[block_num-self.block_offset] = occupied        #Use block offset to set the occupancy
-        self.UIBlockOutput()
         self.setOfficeOccupancy(block_num, occupied)
 
         if(occupied == True):
@@ -62,7 +42,7 @@ class MainWindow(QMainWindow):
         signals.CTC_occupancy.emit("Green",block_num, occupied)    #Sends the Occupancy Signal
 
     #Gets the authority
-    def getAuthority(self, line, block_num):
+    def getAuthority(self, block_num):
         self.authority[block_num-self.block_offset] = False
         self.block_authority = block_num
 
@@ -89,68 +69,25 @@ class MainWindow(QMainWindow):
             signals.wayside_to_track.emit(block_num, 1, self.commanded_speed[block_num-self.block_offset])
     
     #Update the block closure
-    def setBlockClosure(self, line, block_num, break_type):
+    def setBlockClosure(self, block_num, break_type):
         self.block_open[block_num-self.block_offset] = False
         self.UpdateCTCFailure(line, block_num, break_type)
-        self.UIBlockOutput()
     
     #Update the CTC Office of Block Closures
     def UpdateCTCFailure(self, line, block_num, break_type):
         signals.CTC_failure.emit(line, block_num, break_type)
-        self.UIBlockOutput()
 
     #Update the Track model of block openings
     def UpdateTMOpenings(self, line, block_num):
         signals.wayside_block_open.emit(line, block_num)
 
     #Update the block status
-    def UpdateBlockStatus(self, line, block_num, status):
+    def UpdateBlockStatus(self, block_num, status):
         self.block_open[block_num-self.block_offset] = status
         if(status == True):
             self.UpdateTMOpenings(line,block_num)
-        self.UIBlockOutput()
-
-    #Import PLC
-    def ImportPLC(self):
-        self.plc_name = self.ui.ImportLine.currentText()
-        self.ui.SuccessFailLine.setText("Valid File")
-    
-    #Output for the UI
-    def UIBlockOutput(self):
-        if (self.ui.BlockInput.currentText() == "Choose"):
-            self.ui.BlockStatus.setText("N/A")
-            self.ui.Occupancy.setText("N/A")
-            self.ui.Authority.setText("N/A")
-            self.ui.CommandedSpeed.setText("N/A")
-            self.ui.CrossingStatus.setText("N/A")
-            self.ui.SwitchStatus.setText("N/A")
-        else:
-            self.ui_block = int(self.ui.BlockInput.currentText()) #Convert block input to string
-            
-            if(self.block_open[self.ui_block-self.block_offset] == True):
-                self.ui.BlockStatus.setText("Open")
-                self.ui.Occupancy.setText(str(self.occupancy[self.ui_block-self.block_offset]))
-                self.ui.Authority.setText(str(self.authority[self.ui_block-self.block_offset]))
-                self.ui.CommandedSpeed.setText(str(self.commanded_speed[self.ui_block-self.block_offset]))
-                self.ui.CrossingStatus.setText("N/A")
-                self.ui.SwitchStatus.setText("N/A")
-            else:
-                self.ui.BlockStatus.setText("Closed")
-                self.ui.Occupancy.setText("N/A")
-                self.ui.Authority.setText("N/A")
-                self.ui.CommandedSpeed.setText("N/A")
-                self.ui.CrossingStatus.setText("N/A")
-                self.ui.SwitchStatus.setText("N/A")
     
     #Controls the Switch States
     '''
     def ControlSwitch(self):
     '''
-
-if __name__ == "__main__":
-	app = QApplication(sys.argv)
-	
-	window = MainWindow()
-	window.show()
-	
-	sys.exit(app.exec_())
--- DevEnv/src/TrackControllerSW/src/TrackControllerSWInterface.py
+++ DevEnv/src/TrackControllerSW/src/TrackControllerSWInterface.py
@@ -1,9 +1,9 @@
-"""import sys
+import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import QFile
 from .UI import Ui_TrackControllerUI
 from signals import signals
-from TrackControllerSW import TrackControllerSW
+from TrackControllerSW.src.TrackControllerSW import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -12,24 +12,20 @@ class MainWindow(QMainWindow):
         self.ui = Ui_TrackControllerUI()
         self.ui.setupUi(self)
 
-        TrackController Green1
-        TrackController Green2
-        TrackController Green3
-        TrackController Green4
-
-        self.setControllerOffset(Green1,0)
-        self.setControllerOffset(Green2,33)
-        self.setControllerOffset(Green3,74)
-        self.setControllerOffset(Green4,105)
+        self.GreenController1 = TrackController(0)
+        self.GreenController2 = TrackController(33)
+        self.GreenController3 = TrackController(74)
+        self.GreenController4 = TrackController(105)
 
         #UI used variables
         self.ui_block = 0
         self.plc_name = ""
         
         #UI Functions
+        self.ui.StatusLineBox.currentTextChanged.connect(self.UIBlockOutput)
+        self.ui.StatusControllerBox.currentTextChanged.connect(self.UIBlockOutput)
         self.ui.BlockInput.currentTextChanged.connect(self.UIBlockOutput)
         self.ui.ImportButton.clicked.connect(self.ImportPLC)
-
 
         #Signal Functions
         signals.track_model_occupancy.connect(self.getOccupancy)
@@ -39,15 +35,6 @@ class MainWindow(QMainWindow):
         #signals.CTC_suggested_speed.connect(self.getSugSpeed)
         #need wayside to track switch signals
         #need crossing signals
-
-
-        
-
-
-
-    
-    def setControllerOffset(self, track_controller, offset):
-        track_controller.setBlockOffset(offset)
 
     def setFirstSwitchBlocks(self, track_controller, in_a, in_b, end):
         track_controller.switch_1_in_a = in_a
@@ -59,8 +46,133 @@ class MainWindow(QMainWindow):
         track_controller.switch_2_in_b = in_b
         track_controller.switch_2_end = end
 
+    # Occupancy Call
+    def getOccupancy(self, block_num, occupied):
+        if(block_num < 33 or block_num > 146):
+            self.GreenController1.getOccupancy(block_num, occupied)
+        elif(block_num > 32 and block_num < 74):
+            self.GreenController2.getOccupancy(block_num, occupied)
+        elif(block_num > 73 and block_num < 105):
+            self.GreenController3.getOccupancy(block_num, occupied)
+        elif(block_num > 104 and block_num < 147):
+            self.GreenController4.getOccupancy(block_num, occupied)
+        self.UIBlockOutput()
+
+    # Authority Call
+    def getAuthority(self, line, block_num):
+        if(block_num < 33 or block_num > 146):
+            self.GreenController1.getAuthority(block_num)
+        elif(block_num > 32 and block_num < 74):
+            self.GreenController2.getAuthority(block_num)
+        elif(block_num > 73 and block_num < 105):
+            self.GreenController3.getAuthority(block_num)
+        elif(block_num > 104 and block_num < 147):
+            self.GreenController4.getAuthority(block_num)
+
+    # Block Closure
+    def setBlockClosure(self, line, block_num, break_type):
+        if(block_num < 33 or block_num > 146):
+            self.GreenController1.setBlockClosure(block_num, break_type)
+        elif(block_num > 32 and block_num < 74):
+            self.GreenController2.setBlockClosure(block_num, break_type)
+        elif(block_num > 73 and block_num < 105):
+            self.GreenController3.setBlockClosure(block_num, break_type)
+        elif(block_num > 104 and block_num < 147):
+            self.GreenController4.setBlockClosure(block_num, break_type)
+        self.UIBlockOutput()
+    
+    # Block Status Updates
+    def UpdateBlockStatus(self, line, block_num, status):
+        if(block_num < 33 or block_num > 146):
+            self.GreenController1.UpdateBlockStatus(block_num, status)
+        elif(block_num > 32 and block_num < 74):
+            self.GreenController2.UpdateBlockStatus(block_num, status)
+        elif(block_num > 73 and block_num < 105):
+            self.GreenController3.UpdateBlockStatus(block_num, status)
+        elif(block_num > 104 and block_num < 147):
+            self.GreenController4.UpdateBlockStatus(block_num, status)
+        self.UIBlockOutput()
+
     #Output for the UI
     def UIBlockOutput(self):
+        #List of Green Line Controllers
+        green_controllers = ["Choose","1","2","3","4"]
+        print("UIBlockOutput Called\n")
+        if(str(self.ui.StatusLineBox.currentText()) == "Green"):
+
+            #Clear Combo Box
+            #self.ui.StatusControllerBox.clear()
+
+            #Enter Controller Inputs
+            for controller_name in green_controllers:
+                self.ui.StatusControllerBox.addItem(controller_name)
+
+            if(str(self.ui.StatusControllerBox.currentText()) == "1"):
+
+                #list of Blocks
+                controller1_blocks = ["" for i in range(37)]
+                controller1_blocks[0] = "Choose"
+                for i in range(32):
+                    controller1_blocks[i+1] = str(i+1)
+                controller1_blocks[33] = "147"
+                controller1_blocks[34] = "148"
+                controller1_blocks[35] = "149"
+                controller1_blocks[36] = "150"
+
+                #Enter Block Inputs
+                for block_name in controller1_blocks:
+                    self.ui.BlockInput.addItem(block_name)
+ 
+                #Call controller display function
+                self.displayUIOutput(self.GreenController1)
+
+            elif(str(self.ui.StatusControllerBox.currentText()) == "2"):
+
+                #list of Blocks
+                controller2_blocks = ["" for i in range(42)]
+                controller2_blocks[0] = "Choose"
+                for i in range(33,74):
+                    controller2_blocks[i-32] = str(i)
+                
+                #Enter Block Inputs
+                for block_name in controller2_blocks:
+                    self.ui.BlockInput.addItem(block_name)
+ 
+                #Call controller display function
+                self.displayUIOutput(self.GreenController2)
+            
+            elif(str(self.ui.StatusControllerBox.currentText()) == "3"):
+
+                #list of Blocks
+                controller3_blocks = ["" for i in range(32)]
+                controller3_blocks[0] = "Choose"
+                for i in range(74,105):
+                    controller3_blocks[i-73] = str(i)
+                
+                #Enter Block Inputs
+                for block_name in controller3_blocks:
+                    self.ui.BlockInput.addItem(block_name)
+ 
+                #Call controller display function
+                self.displayUIOutput(self.GreenController3)
+
+            elif(str(self.ui.StatusControllerBox.currentText()) == "4"):
+
+                #list of Blocks
+                controller4_blocks = ["" for i in range(43)]
+                controller4_blocks[0] = "Choose"
+                for i in range(105,147):
+                    controller4_blocks[i-104] = str(i)
+            
+                #Enter Block Inputs
+                for block_name in controller4_blocks:
+                    self.ui.BlockInput.addItem(block_name)
+ 
+                #Call controller display function
+                self.displayUIOutput(self.GreenController4)
+
+    #Display the UI functions
+    def displayUIOutput(self, controller):
         if (self.ui.BlockInput.currentText() == "Choose"):
             self.ui.BlockStatus.setText("N/A")
             self.ui.Occupancy.setText("N/A")
@@ -69,13 +181,13 @@ class MainWindow(QMainWindow):
             self.ui.CrossingStatus.setText("N/A")
             self.ui.SwitchStatus.setText("N/A")
         else:
-            self.ui_block = int(self.ui.BlockInput.currentText()) #Convert block input to string
+            controller.ui_block = int(self.ui.BlockInput.currentText()) #Convert block input to string
             
-            if(self.block_open[self.ui_block-self.block_offset] == True):
+            if(controller.block_open[controller.ui_block-controller.block_offset] == True):
                 self.ui.BlockStatus.setText("Open")
-                self.ui.Occupancy.setText(str(self.occupancy[self.ui_block-self.block_offset]))
-                self.ui.Authority.setText(str(self.authority[self.ui_block-self.block_offset]))
-                self.ui.CommandedSpeed.setText(str(self.commanded_speed[self.ui_block-self.block_offset]))
+                self.ui.Occupancy.setText(str(controller.occupancy[controller.ui_block-controller.block_offset]))
+                self.ui.Authority.setText(str(controller.authority[controller.ui_block-controller.block_offset]))
+                self.ui.CommandedSpeed.setText(str(controller.commanded_speed[controller.ui_block-controller.block_offset]))
                 self.ui.CrossingStatus.setText("N/A")
                 self.ui.SwitchStatus.setText("N/A")
             else:
@@ -86,11 +198,15 @@ class MainWindow(QMainWindow):
                 self.ui.CrossingStatus.setText("N/A")
                 self.ui.SwitchStatus.setText("N/A")
     
+    #Import PLC
+    def ImportPLC(self):
+        self.plc_name = self.ui.ImportLine.currentText()
+        self.ui.SuccessFailLine.setText("Valid File")
+
 if __name__ == "__main__":
-app = QApplication(sys.argv)
+    app = QApplication(sys.argv)
 
-window = MainWindow()
-window.show()
+    window = MainWindow()
+    window.show()
 
-sys.exit(app.exec_())    
-    """
+    sys.exit(app.exec_())    
